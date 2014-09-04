7bb88fe8e8b08e193d5fbe9064679901f201a1bc
Raghuveer Subodha <vikram@Raghuveer-Subodha-MacBook-Air.local>
2014-09-04 23:36:19 +0530

    Added UIPageViewControllers

---
+++ SocialSense/SocialSense/UIViewController+pageContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+pageContentViewController.h
+//  SocialSense
+//
+//  Created by Raghuveer Subodha on 04/09/14.
+//  Copyright (c) 2014 Appcellence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (pageContentViewController)
+
+@end
---
+++ SocialSense/SocialSense/UIViewController+pageContentViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+pageContentViewController.m
+//  SocialSense
+//
+//  Created by Raghuveer Subodha on 04/09/14.
+//  Copyright (c) 2014 Appcellence. All rights reserved.
+//
+
+#import "UIViewController+pageContentViewController.h"
+
+@implementation UIViewController (pageContentViewController)
+
+@end
